42fad7544ec14315ea60a54fcd33f857233e0efe
BenJensen2 <btjensen@mtu.edu>
2021-03-03 14:29:37 -0800

    Deployment

--- client/src/Components/ContextMenu.jsx
+++ client/src/Components/ContextMenu.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 // import DispatchForm from '../Components/DispatchForms/DispatchForm';
-import axios from 'axios'
+// import axios from 'axios'
 import '../styles/ContextMenu.css'
 
 const ContextMenu = ({ parentRef, mapRef }) => {
   const [isVisible, setVisibility] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [showIncidentDispatch, setShowIncidentDispatch] = useState(false)
-  const [showMissingPersonsDispatch, setShowMissingPersonsDispatch] = useState(false)
+  // const [showIncidentDispatch, setShowIncidentDispatch] = useState(false)
+  // const [showMissingPersonsDispatch, setShowMissingPersonsDispatch] = useState(false)
 
   useEffect(() => {
     const parent = parentRef.current
@@ -46,24 +46,24 @@ const ContextMenu = ({ parentRef, mapRef }) => {
     left: x
   };
 
-  const createIncident = (e) => {
+  // const createIncident = (e) => {
 
-    let newIncident = {
-      Guest_Name: "Bobby",
-      Responder_Name: "Jim",
-      Incident_Info: "It happened on Bear Peak",
-      Personnel: "Stew, Donny, and some park dude",
-      Gear: "Skis and bindings, No Helmet!!",
-      Features: "The BIG one"
-    }
+  //   let newIncident = {
+  //     Guest_Name: "Bobby",
+  //     Responder_Name: "Jim",
+  //     Incident_Info: "It happened on Bear Peak",
+  //     Personnel: "Stew, Donny, and some park dude",
+  //     Gear: "Skis and bindings, No Helmet!!",
+  //     Features: "The BIG one"
+  //   }
 
-    axios.post("http://localhost:8000/api/incident/new", newIncident)
-      .then(res => {
-        console.log("Incident Created", res.data)
-      }).catch(err => {
-        console.log("We've got errors", err)
-      })
-  }
+  //   axios.post("http://localhost:8000/api/incident/new", newIncident)
+  //     .then(res => {
+  //       console.log("Incident Created", res.data)
+  //     }).catch(err => {
+  //       console.log("We've got errors", err)
+  //     })
+  // }
 
   const incidentDispatch = (e) => {
     console.log("Incident Dispatch")
