240aced2620875941e8d22d6d3ac957bcb10ad9e
BenJensen2 <btjensen@mtu.edu>
2021-12-16 07:55:07 -0600

    Changed allItems api url to item/all

--- server/config/routes.js
+++ server/config/routes.js
@@ -9,13 +9,13 @@ const DispatchController = require("../controllers/DispatchController");
 module.exports = function (app) {
   // MEMBERS
   app.get("/api/index", MemberController.index);
-  app.get("/api/allMembers", MemberController.getMembers);
+  app.get("/api/member/all", MemberController.getMembers);
   app.post("/api/member/new", MemberController.createMember);
   app.get("/api/member/:id", MemberController.getMember);
   app.delete("/api/member/delete/:id", MemberController.deleteMember);
 
   // INCIDENTS
-  app.get("/api/allIncidents", IncidentController.getIncidents);
+  app.get("/api/incident/all", IncidentController.getIncidents);
   app.post("/api/incident/new", IncidentController.createIncident);
   app.get("/api/incident/:id", IncidentController.getIncident);
   app.put("/api/incident/update/:id", IncidentController.updateIncident);
@@ -23,42 +23,42 @@ module.exports = function (app) {
 
   // GUESTS
   app.get("/api/index", GuestController.index);
-  app.get("/api/allGuests", GuestController.getGuests);
+  app.get("/api/guest/all", GuestController.getGuests);
   app.post("/api/guest/new", GuestController.createGuest);
   app.get("/api/guest/:id", GuestController.getGuest);
   app.delete("/api/guest/delete/:id", GuestController.deleteGuest);
 
   // CONTACTS
   app.get("/api/index", GuestController.index);
-  app.get("/api/allGuests", GuestController.getGuests);
+  app.get("/api/guest/all", GuestController.getGuests);
   app.post("/api/guest/new", GuestController.createGuest);
   app.get("/api/guest/:id", GuestController.getGuest);
   app.delete("/api/guest/delete/:id", GuestController.deleteGuest);
 
   // RUNS
   app.get("/api/index", RunController.index);
-  app.get("/api/allRuns", RunController.getRuns);
+  app.get("/api/run/all", RunController.getRuns);
   app.post("/api/run/new", RunController.createRun);
   app.get("/api/run/:id", RunController.getRun);
   app.delete("/api/run/delete/:id", RunController.deleteRun);
 
   // LIFTS
   app.get("/api/index", LiftController.index);
-  app.get("/api/allLifts", LiftController.getLifts);
+  app.get("/api/lift/all", LiftController.getLifts);
   app.post("/api/lift/new", LiftController.createLift);
   app.get("/api/lift/:id", LiftController.getLift);
   app.delete("/api/lift/delete/:id", LiftController.deleteLift);
 
   // FEATURES
   app.get("/api/index", FeatureController.index);
-  app.get("/api/allFeatures", FeatureController.getFeatures);
+  app.get("/api/feature/all", FeatureController.getFeatures);
   app.post("/api/feature/new", FeatureController.createFeature);
   app.get("/api/feature/:id", FeatureController.getFeature);
   app.delete("/api/feature/delete/:id", FeatureController.deleteFeature);
 
   // DISPATCHES
   app.get("/api/index", DispatchController.index);
-  app.get("/api/allDispatches", DispatchController.getDispatches);
+  app.get("/api/dispatch/all", DispatchController.getDispatches);
   app.post("/api/dispatch/new", DispatchController.createDispatch);
   app.get("/api/dispatch/:id", DispatchController.getDispatch);
   app.delete("/api/dispatch/delete/:id", DispatchController.deleteDispatch);
